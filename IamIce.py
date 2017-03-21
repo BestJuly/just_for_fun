908461ce5ab9872288edfc62a490f1c8e06bace6
li tao <chntaoli@pku.edu.cn>
2017-03-21 18:34:41 +0800

    link your wechat to XiaoIce

---
+++ IamIce.py
@@ -0,0 +1,21 @@
+﻿#coding=utf-8
+import itchat
+
+global name
+
+@itchat.msg_register(itchat.content.TEXT, True, False, False)
+def send_xiaoice(msg):
+  global name
+  name = msg['FromUserName']
+  itchat.send(msg['Text'],toUserName='xiaoice-ms')
+  
+@itchat.msg_register(itchat.content.TEXT, False, False, True)
+def send_reply(msg):
+  global name
+  if msg['Text']:
+    itchat.send(msg['Text'],name)
+  else:
+    itchat.send('...',name)
+
+itchat.auto_login(hotReload=True)
+itchat.run()
