476b7cbfaa4574e5ae05696b1ba1249401e3cb2d
li tao <li tao>
2017-03-21 19:35:54 +0800

    support images

--- IamIce.py
+++ IamIce.py
@@ -1,21 +1,32 @@
 ﻿#coding=utf-8
 import itchat
-
+from itchat.content import *
+import pdb
 global name
 
-@itchat.msg_register(itchat.content.TEXT, True, False, False)
+@itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING], True, False, False) # get text and send to XiaoIce
 def send_xiaoice(msg):
   global name
   name = msg['FromUserName']
   itchat.send(msg['Text'],toUserName='xiaoice-ms')
   
-@itchat.msg_register(itchat.content.TEXT, False, False, True)
+@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO], True, False, False) # get img and send to XiaoIce
+def send_xiaoice(msg):
+  global name
+  name = msg['FromUserName']
+  msg['Text'](msg['FileName'])
+  itchat.send('@%s@%s' % ({'Picture': 'img', 'Video': 'vid'}.get(msg['Type'], 'fil'), msg['FileName']), toUserName='xiaoice-ms')
+
+@itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING], False, False, True) # get text and send to Sender
 def send_reply(msg):
   global name
-  if msg['Text']:
-    itchat.send(msg['Text'],name)
-  else:
-    itchat.send('...',name)
+  itchat.send(msg['Text'],name)
+
+@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO], False, False, True) # get img and send to Sender
+def send_xiaoice(msg):
+  global name
+  msg['Text'](msg['FileName'])
+  itchat.send('@%s@%s' % ({'Picture': 'img', 'Video': 'vid'}.get(msg['Type'], 'fil'), msg['FileName']), name)
 
 itchat.auto_login(hotReload=True)
 itchat.run()
