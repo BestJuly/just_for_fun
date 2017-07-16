c16486123c8493b2317c043745fb4a3e7c6ee7ad
li tao <li tao>
2017-07-17 00:28:00 +0800

    update

--- download_CVPR2017.py
+++ download_CVPR2017.py
@@ -8,18 +8,23 @@ r = requests.get('http://openaccess.thecvf.com/CVPR2017.py')
 data = r.text
 # find all pdf links
 link_list =re.findall(r"(?<=href=\").+?pdf\">pdf|(?<=href=\').+?pdf\">pdf" ,data)
+name_list =re.findall(r"(?<=href=\").+?2017_paper.html\">.+?</a>" ,data)
+cnt = 0
 # your local path to download pdf files
 localDir = 'E:\CVPR2017\\'
 if not os.path.exists(localDir):
     os.makedirs(localDir)
 for url in link_list:
+    url = url[0:-5]
     #seperate file name from url links
-    file_name = url.split('/')[2].split('.')[0]
+    file_name = name_list[cnt].split('<')[0].split('>')[1]
     file_path = localDir + file_name + '.pdf'
     # download pdf files
     try:
         urllib.urlretrieve('http://openaccess.thecvf.com/'+url,file_path)
+        # os.symtem('wget '+url+' -O '+file_path)
         print "downloading:"+url+" -> "+file_path  
     except Exception,e:
         continue
+    cnt = cnt + 1
 print "all download finished"  
