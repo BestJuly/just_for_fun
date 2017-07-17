481744b99d35e2d79dbbd1ec50e0ab3bcf4c12a8
li tao <li tao>
2017-07-17 23:02:20 +0800

    rectify code

--- download_CVPR2017.py
+++ download_CVPR2017.py
@@ -7,27 +7,28 @@ import os
 r = requests.get('http://openaccess.thecvf.com/CVPR2017.py')
 data = r.text
 # find all pdf links
-link_list =re.findall(r"(?<=href=\").+?pdf\">pdf|(?<=href=\').+?pdf\">pdf" ,data)
+link_list =re.findall(r"(?<=href=\").+?pdf(?=\">pdf)|(?<=href=\').+?pdf(?=\">pdf)" ,data)
 name_list =re.findall(r"(?<=href=\").+?2017_paper.html\">.+?</a>" ,data)
 cnt = 0
+num = len(link_list)
 # your local path to download pdf files
 localDir = 'E:\CVPR2017\\'
 if not os.path.exists(localDir):
     os.makedirs(localDir)
-for url in link_list:
-    url = url[0:-5]
-    #seperate file name from url links
+while cnt < num:
+    url = link_list[cnt]
+    # seperate file name from url links
     file_name = name_list[cnt].split('<')[0].split('>')[1]
-    #import pdb
-    #pdb.set_trace()
+    # to avoid some illegal punctuation in file name
     file_name = file_name.replace(':','_')
     file_name = file_name.replace('\"','_')
+    file_name = file_name.replace('?','_')
+    file_name = file_name.replace('/','_')
     file_path = localDir + file_name + '.pdf'
     # download pdf files
+    print '['+str(cnt)+'/'+str(num)+"]  Downloading -> "+file_path
     try:
         urllib.urlretrieve('http://openaccess.thecvf.com/'+url,file_path)
-        # os.symtem('wget '+url+' -O '+file_path)
-        print "downloading:"+url+" -> "+file_path  
     except Exception,e:
         continue
     cnt = cnt + 1
